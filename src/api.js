e4f14af03881a91e62c904b011ba2fff380dae07
HARIHARASUDAN <saravananhari2006@gmail.com>
2025-08-14 20:18:52 +0530

    updated

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "http://my-json-server.typicode.com/HARIHARASUDAN15/todo-crud",
   headers: { "Content-Type": "application/json" }
 });
 
